994b7b93197d943afd68408a7e41b6fe5c8d9ae2
Volodymyr Makhitko <volodymyr.makhitko@globallogic.com>
2016-07-19 22:10:37 +0200

    Update Test allow services
    Signed-off-by: Volodymyr Makhitko <volodymyr.makhitko@globallogic.com>

--- 0003_test_allow_services.py
+++ 0003_test_allow_services.py
@@ -71,23 +71,36 @@ def processing(shell, nc):
             print "TEST FAILED"
             sys.exit()
 
+def init_nc(ssh_nc, s_nc):
+    shell = ssh.invoke_shell()
+    shell.send(ssh_nc)
+    shell.recv(2024)
+
+    time.sleep(1)
+
+    nc = subprocess.Popen(s_nc,
+                          stdout=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+
+    processing(shell, nc)
+
+
 
 #test all ports
 for i in range(len(services)):
-    print "Port: " + services[i][0] + " : " + services[i][1]
     #exec ssh command & subprocess
+    #TODO: replace by function init_nc
+    #left only commands
     if not services[i][1]:
-        print "Debug: " + nc_ + listen_param + services[i][0]
         shell = ssh.invoke_shell()
         print shell.recv(2024)
         cmd = nc_ + " " + listen_param + " "+ services[i][0] + '\n'
-        print "cmd: " + cmd
         shell.send(cmd)
-        print "nc " + shell.recv(2024)
+        shell.recv(2024)
 
         time.sleep(1)
 
-        print "Debug subproc: " + nc_ + " " + target_ip + " " + services[i][0]
         nc = subprocess.Popen([nc_, target_ip, services[i][0]],
                               stdout=subprocess.PIPE,
                               stdin=subprocess.PIPE,
@@ -99,7 +112,6 @@ for i in range(len(services)):
         shell.close()
 
     else:
-        print "Debug ssh: " + nc_obd + udp_param + listen_param + services[i][0]
         shell = ssh.invoke_shell()
         cmd = nc_ + ' -v ' + udp_param + " " + listen_param + " " +  services[i][0] +"\n"
         shell.send(cmd)
@@ -107,7 +119,6 @@ for i in range(len(services)):
 
         time.sleep(1)
 
-        print "Debug subproc: " + nc_ + udp_param + target_ip + services[i][0]
         cmd = [nc_, udp_param, target_ip, services[i][0]]
 
         nc = subprocess.Popen([nc_, udp_param, target_ip, services[i][0]],
@@ -115,7 +126,6 @@ for i in range(len(services)):
                               stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE)
 
-        time.sleep(0.1)
         processing(shell, nc)
         nc.kill()
         shell.close()
